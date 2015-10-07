346631cd16483e914c09f142ce61ed7a172eca90
Florent Bécart <florent.becart@gmail.com>
2015-10-07 16:45:52 +0200

    Use Flux Challenge CSS stylesheet

--- src/clj/flux_challenge_reagent/handler.clj
+++ src/clj/flux_challenge_reagent/handler.clj
@@ -15,7 +15,7 @@
      [:meta {:charset "utf-8"}]
      [:meta {:name "viewport"
              :content "width=device-width, initial-scale=1"}]
-     (include-css (if (env :dev) "css/site.css" "css/site.min.css"))]
+     (include-css (if (env :dev) "css/styles.css" "css/styles.min.css"))]
     [:body
      [:div#app
       [:h3 "ClojureScript has not been compiled!"]
