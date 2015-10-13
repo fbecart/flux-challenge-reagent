5285abf53566a8c2f249ffdacf033453c40a519d
Florent Bécart <florent.becart@gmail.com>
2015-10-13 15:53:00 +0200

    Implement scroll up

--- src/cljs/flux_challenge_reagent/core.cljs
+++ src/cljs/flux_challenge_reagent/core.cljs
@@ -21,6 +21,7 @@
 (defonce current-planet (r/atom {}))
 (defonce sith-lords-list (r/atom (vec (repeat list-size nil))))
 (defonce next-apprentice-request (r/atom nil))
+(defonce next-master-request (r/atom nil))
 
 ;; -------------------------
 ;; Views
@@ -40,7 +41,7 @@
      (map render-sith-lord @sith-lords-list)]]
 
    [:div.css-scroll-buttons
-    [:button.css-button-up]
+    [:button.css-button-up {:on-click scroll-up!}]
     [:button.css-button-down {:on-click scroll-down!}]]])
 
 (defn current-page []
@@ -74,31 +75,43 @@
 
 ;; -------------------------
 ;; Sith Lords list
-(defn add-apprentice [sith-lords-list apprentice]
-  (let [index-of-nil-tail (- list-size (util/nil-head-count (reverse sith-lords-list)))]
-    (assoc sith-lords-list index-of-nil-tail apprentice)))
+(defn handle-next-apprentice! [apprentice]
+  (reset! next-apprentice-request nil)
+  (swap! sith-lords-list
+         #(util/reverse-vec-fn util/replace-nil-head-end % apprentice))
+  (if (nil? (peek @sith-lords-list))
+    (load-next-apprentice)))
 
-(defn request-next-apprentice [url]
-  (swap! next-apprentice-request
+(defn handle-next-master! [master]
+  (reset! next-master-request nil)
+  (swap! sith-lords-list util/replace-nil-head-end master)
+  (if (nil? (first @sith-lords-list))
+    (load-next-master)))
+
+(defn request-sith-lord [request-atom url handler]
+  (swap! request-atom
          (fn [current-request]
            (if (nil? current-request)
-             (http/get-json url handle-next-apprentice!)
+             (http/get-json url handler)
              current-request))))
 
 (defn load-next-apprentice []
   (let [last-sith-lord (first (filter some? (reverse @sith-lords-list)))]
     (if-let [apprentice-url (get-in last-sith-lord ["apprentice" "url"])]
-      (request-next-apprentice apprentice-url))))
+      (request-sith-lord next-apprentice-request apprentice-url handle-next-apprentice!))))
 
-(defn handle-next-apprentice! [apprentice]
-  (reset! next-apprentice-request nil)
-  (swap! sith-lords-list add-apprentice apprentice)
-  (if (nil? (peek @sith-lords-list))
-    (load-next-apprentice)))
+(defn load-next-master []
+  (let [first-sith-lord (first (filter some? @sith-lords-list))
+        master-url (get-in first-sith-lord ["master" "url"])]
+    (request-sith-lord next-master-request master-url handle-next-master!)))
 
 (defn scroll-down! []
   (swap! sith-lords-list util/slide scroll-range)
   (load-next-apprentice))
+
+(defn scroll-up! []
+  (swap! sith-lords-list #(util/reverse-vec-fn util/slide % scroll-range))
+  (load-next-master))
 
 ;; -------------------------
 ;; Initialize app
@@ -109,4 +122,4 @@
   (hook-browser-navigation!)
   (mount-root)
   (subscribe-to-current-planet!)
-  (request-next-apprentice darth-sidious-url))
+  (request-sith-lord next-apprentice-request darth-sidious-url handle-next-apprentice!))
--- src/cljs/flux_challenge_reagent/util.cljs
+++ src/cljs/flux_challenge_reagent/util.cljs
@@ -5,3 +5,10 @@
 
 (defn slide [v n]
   (vec (concat (subvec v n) (repeat n nil))))
+
+(defn reverse-vec-fn [f v & params]
+  (vec (reverse (apply f (conj params (vec (reverse v)))))))
+
+(defn replace-nil-head-end [coll x]
+  (let [nil-head-end-index (- (nil-head-count coll) 1)]
+    (assoc coll nil-head-end-index x)))
