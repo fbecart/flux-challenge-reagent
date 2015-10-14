75e6a7bb868e20972cc9aac7c47b4f103661c04f
Florent Bécart <florent.becart@gmail.com>
2015-10-14 17:30:46 +0200

    Store URL and request of loading Sith Lords

--- src/cljs/flux_challenge_reagent/sith_lords.cljs
+++ src/cljs/flux_challenge_reagent/sith_lords.cljs
@@ -5,54 +5,58 @@
 
 ;; -------------------------
 ;; Constant parameters
-(def darth-sidious-url "http://localhost:3000/dark-jedis/3616")
+(def darth-sidious-id 3616)
 
 ;; -------------------------
 ;; State
-(defonce coll (r/atom (repeat 5 nil)))
-(defonce next-apprentice-request (r/atom nil))
-(defonce next-master-request (r/atom nil))
+(defonce coll (r/atom nil))
 
 ;; -------------------------
 ;; Sith Lords list
-(defn handle-next-apprentice! [apprentice]
-  (reset! next-apprentice-request nil)
-  (swap! coll
-         #(util/reverse-seq-fn util/replace-nil-head-end % {:url (:url apprentice) :sith-lord apprentice}))
-  (if (nil? (peek (vec @coll)))
-    (load-next-apprentice)))
+(defn sith-lord-url [id]
+  (str "http://localhost:3000/dark-jedis/" id))
 
-(defn handle-next-master! [master]
-  (reset! next-master-request nil)
-  (swap! coll util/replace-nil-head-end {:url (:url master) :sith-lord master})
-  (if (nil? (first @coll))
-    (load-next-master)))
+(defn sith-lord-item [sith-lord]
+  {:id (get sith-lord "id")
+   :sith-lord sith-lord})
 
-(defn request-sith-lord [request-atom url handler]
-  (swap! request-atom
-         (fn [current-request]
-           (if (nil? current-request)
-             (http/get-json url handler)
-             current-request))))
+(defn sith-lord-request-item! [id]
+  (if id
+    (let [url (sith-lord-url id)]
+      {:id id
+       :request (http/get-json url handle-sith-lord-response!)})
+    nil))
 
-(defn load-next-apprentice []
-  (let [last-sith-lord (:sith-lord (first (filter some? (reverse @coll))))]
-    (if-let [apprentice-url (get-in last-sith-lord ["apprentice" "url"])]
-      (request-sith-lord next-apprentice-request apprentice-url handle-next-apprentice!))))
+(defn request-sith-lord! [coll index id]
+  (if (and (>= index 0) (< index (count coll)) id (nil? (get coll index)))
+    (assoc coll index (sith-lord-request-item! id))
+    coll))
 
-(defn load-next-master []
-  (let [first-sith-lord (:sith-lord (first (filter some? @coll)))
-        master-url (get-in first-sith-lord ["master" "url"])]
-    (request-sith-lord next-master-request master-url handle-next-master!)))
+(defn accept-sith-lord! [coll sith-lord]
+  (let [sith-lord-id (get sith-lord "id")
+        sith-lord-index (util/index-of (map :id coll) sith-lord-id)]
+    (-> (vec coll)
+        (assoc sith-lord-index (sith-lord-item sith-lord))
+        (request-sith-lord! (inc sith-lord-index) (get-in sith-lord ["apprentice" "id"]))
+        (request-sith-lord! (dec sith-lord-index) (get-in sith-lord ["master" "id"])))))
+
+(defn handle-sith-lord-response! [sith-lord]
+  (swap! coll accept-sith-lord! sith-lord))
+
+(defn scroll-once! [coll direction]
+  (case direction
+    :up (let [first-sith-lord (-> coll first :sith-lord)
+              master-id (get-in first-sith-lord ["master" "id"])
+              master-request (sith-lord-request-item! master-id)]
+          (into [master-request] (pop coll)))
+    :down (let [last-sith-lord (-> coll last :sith-lord)
+                apprentice-id (get-in last-sith-lord ["apprentice" "id"])
+                apprentice-request (sith-lord-request-item! apprentice-id)]
+            (conj (subvec coll 1) apprentice-request))))
 
 (defn scroll! [n direction]
-  (case direction
-    :up (do
-          (swap! coll #(util/reverse-seq-fn util/slide % n))
-          (load-next-master))
-    :down (do
-            (swap! coll util/slide n)
-            (load-next-apprentice))))
+  (swap! coll (fn [coll]
+                (reduce #(scroll-once! %1 direction) coll (range n)))))
 
 (defn init! []
-  (request-sith-lord next-apprentice-request darth-sidious-url handle-next-apprentice!))
+  (reset! coll (request-sith-lord! (vec (repeat 5 nil)) 0 darth-sidious-id)))
--- src/cljs/flux_challenge_reagent/util.cljs
+++ src/cljs/flux_challenge_reagent/util.cljs
@@ -1,14 +1,7 @@
 (ns flux-challenge-reagent.util)
 
-(defn nil-head-count [coll]
-  (count (take-while nil? coll)))
-
-(defn slide [coll n]
-  (subvec (vec (concat coll (repeat n nil))) n))
-
-(defn reverse-seq-fn [f coll & params]
-  (reverse (apply f (conj params (reverse coll)))))
-
-(defn replace-nil-head-end [coll x]
-  (let [nil-head-end-index (- (nil-head-count coll) 1)]
-    (assoc (vec coll) nil-head-end-index x)))
+(defn index-of [coll v]
+  (let [i (count (take-while #(not= v %) coll))]
+    (when (or (< i (count coll))
+            (= v (last coll)))
+      i)))
