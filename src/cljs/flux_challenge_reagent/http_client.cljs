35cef9a83db7f458944abbd70acb2b2485c14e02
Florent Bécart <florent.becart@gmail.com>
2015-10-16 15:53:35 +0200

    Abort requests of Sith Lords outside the list when user scrolls

--- src/cljs/flux_challenge_reagent/http_client.cljs
+++
@@ -1,5 +0,0 @@
-(ns flux-challenge-reagent.http-client
-    (:require [ajax.core :refer [GET]]))
-
-(defn get-json [url handler]
-  (GET url :response-format :json :handler handler))
--- src/cljs/flux_challenge_reagent/sith_lords.cljs
+++ src/cljs/flux_challenge_reagent/sith_lords.cljs
@@ -1,6 +1,6 @@
 (ns flux-challenge-reagent.sith-lords
     (:require [reagent.core :as r]
-              [flux-challenge-reagent.http-client :as http]
+              [ajax.core :as ajax]
               [flux-challenge-reagent.util :as util]))
 
 ;; -------------------------
@@ -24,7 +24,9 @@
   (if id
     (let [url (sith-lord-url id)]
       {:id id
-       :request (http/get-json url handle-sith-lord-response!)})
+       :request (ajax/GET url
+                          :response-format :json
+                          :handler handle-sith-lord-response!)})
     nil))
 
 (defn request-sith-lord! [coll index id]
@@ -43,15 +45,21 @@
 (defn handle-sith-lord-response! [sith-lord]
   (swap! coll accept-sith-lord! sith-lord))
 
+(defn abort-request! [item]
+  (if-let [request (:request item)]
+    (ajax/abort request)))
+
 (defn scroll-once! [coll direction]
   (case direction
     :up (let [first-sith-lord (-> coll first :sith-lord)
               master-id (get-in first-sith-lord ["master" "id"])
               master-request (sith-lord-request-item! master-id)]
+          (abort-request! (peek coll))
           (into [master-request] (pop coll)))
     :down (let [last-sith-lord (-> coll last :sith-lord)
                 apprentice-id (get-in last-sith-lord ["apprentice" "id"])
                 apprentice-request (sith-lord-request-item! apprentice-id)]
+            (abort-request! (nth coll 0))
             (conj (subvec coll 1) apprentice-request))))
 
 (defn scroll! [n direction]
