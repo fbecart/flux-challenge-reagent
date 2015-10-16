e400c2941f910051826b302606089be771087a38
Florent Bécart <florent.becart@gmail.com>
2015-10-16 16:37:13 +0200

    Restore function core/mount-root
    It is referenced in env/dev/cljs/flux_challenge_reagent/dev.cljs

--- src/cljs/flux_challenge_reagent/core.cljs
+++ src/cljs/flux_challenge_reagent/core.cljs
@@ -4,7 +4,10 @@
             [flux-challenge-reagent.current-planet :as current-planet]
             [flux-challenge-reagent.sith-lords :as sith-lords]))
 
+(defn mount-root []
+  (r/render-component [view/dashboard] (.getElementById js/document "app")))
+
 (defn init! []
-  (r/render-component [view/dashboard] (.getElementById js/document "app"))
+  (mount-root)
   (current-planet/init-websocket-connection!)
   (sith-lords/init!))
